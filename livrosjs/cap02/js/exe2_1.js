88e099704d9f4a63c3e7791ba409f7fc4a93e3d7
Marks Aurelio <marksaurelio@yahoo.com>
2024-09-24 21:50:04 -0300

    exemplo 2 do cap02

---
+++ livrosjs/cap02/js/exe2_1.js
@@ -0,0 +1,10 @@
+// criar uma referência ao form e ao h3 (onde será exibida a resposta)
+const frm = document.querySelector("form");
+const resp = document.querySelector("h3");
+
+// cria um 'ouvinte', espera uma ação em um determinado lugar, acionando quando o botão submit for clicado
+frm.addEventListener("submit", (e)=>{
+    const nome = frm.inNome.value; // obtém o nome digitado no form
+    resp.innerText = `Olá ${nome}`;
+    e.preventDefault(); // vai enviar que o form enviou os dados pro destino
+})
