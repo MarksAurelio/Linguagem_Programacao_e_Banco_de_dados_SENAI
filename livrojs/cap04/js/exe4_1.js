6b6865b9da51d4a1059d62c00291a4d8780358e0
Marks Aurelio <marksaurelio@yahoo.com>
2024-10-01 21:36:52 -0300

    atualizacao do exe4_1

--- livrojs/cap04/js/exe4_1.js
+++ livrojs/cap04/js/exe4_1.js
@@ -4,24 +4,27 @@ const resp1 = document.querySelector("h3")
 const resp2 = document.querySelector("h4")
 
 // cria um "ouvinte" de evento, acionado quando o botão submit for clicado
-frm.addEventListener("submit", (e)=>{
+frm.addEventListener("submit", (e) => {
     const nome = frm.inNome.value // obtém valores do form
     const nota1 = Number(frm.inNota1.value)
     const nota2 = Number(frm.inNota2.value)
     const media = (nota1 + nota2) / 2 // calcula a média das notas
-    
-    resp1.innerText= `Média das notas ${media.toFixed(2)}`
+
+    resp1.innerText = `Média das notas ${media.toFixed(2)}`
     // cria as condições
     if (media >= 7) {
-    // altera o texto e estilo da cor do elemento resp2
-    resp2.innerText = `Parabéns ${nome}! Você foi aprovado(a)`
-    resp2.style.color = "blue"}
+        // altera o texto e estilo da cor do elemento resp2
+        resp2.innerText = `Parabéns ${nome}! Você foi aprovado(a)`
+        resp2.style.color = "blue"
+    }
     else if (media >= 4) {
         resp2.innerText = `Atenção ${nome}. Você está em exame`
-        resp2.style.color = "green"}
+        resp2.style.color = "green"
+    }
     else {
-    resp2.innerText = `Ops ${nome}... Você foi reprovado(a)`
-    resp2.style.color = "red"}
+        resp2.innerText = `Ops ${nome}... Você foi reprovado(a)`
+        resp2.style.color = "red"
+    }
 
     e.preventDefault() // evita envio do form
 })
