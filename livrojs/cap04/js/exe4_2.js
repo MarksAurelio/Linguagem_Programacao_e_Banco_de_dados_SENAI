dca4c80b69fe0e553e9a9cbf572865f301ddfc0e
Marks Aurelio <marksaurelio@yahoo.com>
2024-10-03 20:28:19 -0300

    outra solucao para o exe4 do Cap04

--- livrojs/cap04/js/exe4_2.js
+++ livrojs/cap04/js/exe4_2.js
@@ -1,10 +1,10 @@
-
 function calcular() {
     const nome = document.getElementById("inNome").value
     const masculino = document.getElementById("inMasculino").checked
     const altura = document.getElementById("inAltura").value
 
     let peso
+    // ou let peso = masculino ? 22 * Math.pow(altura, 2) : 21 * Math.pow(altura, 2) // essa forma Operador Ternário
 
     if (masculino) {
         peso = 22 * Math.pow(altura, 2)
@@ -12,28 +12,34 @@ function calcular() {
         peso = 21 * Math.pow(altura, 2)
     }
 
-    document.querySelector("h3").innerText = `Peso ideal ${peso.toFixed(2)}`
+    document.querySelector("h3").innerText = `${nome}: Peso ideal ${peso.toFixed(2)}`
 
     return false // vai impedir o refresh da página equivalente ao e.preventDefault()
 }
 
-/*
-const frm = document.querySelector('form')
-const resp = document.querySelector('h3')
+function limpar() {
+    document.querySelector("h3").innerText = ""
 
-frm.addEventListener('submit', (e) => {
-    e.preventDefault()
+}
 
-    const nome = frm.inNome.value
-    const masculino = frm.inMasculino.value.checked
-    const altura = Number(frm.inAltura.value)
+// const frm = document.querySelector('form')
+// const resp = document.querySelector('h3')
 
-    let peso
-    if (masculino) { // se masculino (ou, if masculino == true)
-        peso = 22 * Math.pow(altura, 2)
-    }
-    else { peso = 21 * Math.pow(altura, 2) }
+// frm.addEventListener('submit', (e) => {
+//     frm.addEventListener("reset", () => {
+//         resp.innerText = "" // limpa o conteúdo do elemento h3 que exibe a resposta
+//     })
+//     e.preventDefault()
 
-    resp.innerText = `${nome}: Seu peso ideal é ${peso.toFixed(3)} Kg`
-})
-    */
+//     const nome = frm.inNome.value
+//     const masculino = frm.inMasculino.value.checked
+//     const altura = Number(frm.inAltura.value)
+
+//     let peso
+//     if (masculino) { // se masculino (ou, if masculino == true)
+//         peso = 22 * Math.pow(altura, 2)
+//     }
+//     else { peso = 21 * Math.pow(altura, 2) }
+
+//     resp.innerText = `${nome}: Seu peso ideal é ${peso.toFixed(3)} Kg`
+// })
