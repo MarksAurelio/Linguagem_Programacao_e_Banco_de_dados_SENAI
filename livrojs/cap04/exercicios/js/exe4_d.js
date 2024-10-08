387d7cbb421fc2db43319f4d3e2e56541623dd9e
Marks Aurelio <marksaurelio@yahoo.com>
2024-10-08 08:03:22 -0300

    exercicio d Programa Lado de um Triangulo

---
+++ livrojs/cap04/exercicios/js/exe4_d.js
@@ -0,0 +1,20 @@
+const frm = document.querySelector('form')
+const resp1 = document.querySelector('h3')
+const resp2 = document.querySelector('h4')
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const ladoA = Number(frm.inLadoA.value)
+    const ladoB = Number(frm.inLadoB.value)
+    const ladoC = Number(frm.inLadoC.value)
+
+    if (ladoA === ladoB && ladoB === ladoC) {
+        alert(`Lados podem formar um triângulo \nTipo: Equilátero`)
+    }
+    else if (ladoA === ladoB || ladoA === ladoC || ladoB === ladoC) {
+        alert(`Lados podem formar um triângulo \nTipo: Isósceles`)
+    }
+    else {
+        alert(`Lados podem formar um triângulo \nTipo: Escaleno`)
+    }
+})
