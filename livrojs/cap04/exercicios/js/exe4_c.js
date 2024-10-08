80de3d0378bb051590b440edadf5d81db52f9cae
Marks Aurelio <marksaurelio@yahoo.com>
2024-10-08 00:06:50 -0300

    exercicio c Cap04

--- livrojs/cap04/exercicios/js/exe4_c.js
+++ livrojs/cap04/exercicios/js/exe4_c.js
@@ -2,21 +2,20 @@ const frm = document.querySelector('form')
 const resp1 = document.querySelector('h3')
 const resp2 = document.querySelector('h4')
 
-frm.addEventListener("submit", (e)=>{
+frm.addEventListener("submit", (e) => {
     e.preventDefault()
     const valor = Number(frm.inValor.value)
 
-
-    if(valor > 1 && valor < 1.75){
-        alert(`Tempo: 30 min \n ${troco = valor - 1}`)
+    if (valor > 1 && valor < 1.75) {
+        alert(`Tempo: 30 min \n Troco:${troco = valor - 1}`)
     }
-    else if(valor > 1.75 && valor < 3){
-        alert(`Tempo: 60 min `)
+    else if (valor > 1.75 && valor < 3) {
+        alert(`Tempo: 60 min \n Troco:${troco = valor - 1.75}`)
     }
-    else if(valor >= 3){
-        alert(`Tempo: 120 min `)
+    else if (valor >= 3) {
+        alert(`Tempo: 120 min \n Troco:${troco = valor - 3}`)
     }
-    else{
+    else {
         alert("Tempo insuficiente!")
     }
 })
