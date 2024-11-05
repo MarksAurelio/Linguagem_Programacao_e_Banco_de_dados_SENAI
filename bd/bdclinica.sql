2e5d064030d606944d3c2946a66e67b7c10cf5e5
Marks Aurelio <marksaurelio@yahoo.com>
2024-11-04 21:46:39 -0300

    criacao exemplo dbcampeonatobrasileiro_estadios

--- bd/bdclinica.sql
+++ bd/bdclinica.sql
@@ -2,34 +2,45 @@ create database dbclinica;
 use dbclinica;
 
 create table sala(
-numero_sala int auto_increment not null unique check(> 1 and < 50),
-andar int not null check(< 12)
-constraint PK_Sala primary key(numero_sala));
+numero_sala int check(numero_sala > 1 and numero_sala < 50),
+andar int not null check(andar < 12),
+crm int not null,
+constraint PK_Sala primary key(numero_sala),
+constraint FK_SalaMedicos foreign key (crm) references medicos(crm));
 
 create table medicos(
-crm int auto_increment not null,
+crm int,
 nome varchar(40) not null,
-idade int check(> 23),
+idade int check(idade > 23),
 especialidade varchar(20) not null default('Ortopedia'),
 cpf varchar(15) unique not null,
 data_admissao date);
 
 create table pacientes(
 cpf varchar(11) auto_increment unique not null,
-nome varchar(40) not null
-data_nascimento date
+nome varchar(40) not null,
+data_nascimento date,
 cidade varchar(30) default ('Taguatinga'),
 doenca varchar(40) not null,
 plano_saude varchar(40) not null default ('SUS'),
+constraint PK_Pacientes primary key(cpf));
 
 create table funcionarios(
-matricula int unique not null auto_increment,
+matricula int auto_increment,
 nome varchar(40) not null,
+data_nascimento date not null,
 data_admissao date not null,
-cargo varchar(40) not null default (15.510,00));
+cargo varchar(40) not null default ('Assistente Médico'));
+salario_decimal(8,2) not null default(1510.00),
+constraint PK_Funcionario primary key(matricula));
 
 create table consultas(
-codigo_consulta int unique not null auto_increment,
-data_horario datetime);
+codigo_consulta int auto_increment,
+data_horario datetime),
+cpf varchar(11) not null,
+crm int not null,
+constraint PK_Consultas primary key(codico_consulta),
+constraint FK_ConsultasMedicos foreign key (crm) references medicos(crm),
+constraint FK_ConsultasPacientes foreign key (cpf) references paciente(cpf));
 
 
