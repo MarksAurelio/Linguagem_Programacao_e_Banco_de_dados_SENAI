060edc08e53abe428121afa4c799af5a0620950c
Marks Aurelio <marksaurelio@yahoo.com>
2024-11-05 21:45:07 -0300

    atualizacao exercicio dbcampeonatobrasileiro

--- bd/bdclinica.sql
+++ bd/bdclinica.sql
@@ -14,10 +14,11 @@ nome varchar(40) not null,
 idade int check(idade > 23),
 especialidade varchar(20) not null default('Ortopedia'),
 cpf varchar(15) unique not null,
-data_admissao date);
+data_admissao date,
+constraint PK_Medicos primary key(crm));
 
 create table pacientes(
-cpf varchar(11) auto_increment unique not null,
+cpf varchar(11),
 nome varchar(40) not null,
 data_nascimento date,
 cidade varchar(30) default ('Taguatinga'),
@@ -30,17 +31,17 @@ matricula int auto_increment,
 nome varchar(40) not null,
 data_nascimento date not null,
 data_admissao date not null,
-cargo varchar(40) not null default ('Assistente Médico'));
-salario_decimal(8,2) not null default(1510.00),
+cargo varchar(40) not null default ('Assistente Médico'),
+salario_decimal decimal(8,2) not null default(1510.00),
 constraint PK_Funcionario primary key(matricula));
 
 create table consultas(
 codigo_consulta int auto_increment,
-data_horario datetime),
+data_horario datetime,
 cpf varchar(11) not null,
 crm int not null,
-constraint PK_Consultas primary key(codico_consulta),
+constraint PK_Consultas primary key(codigo_consulta),
 constraint FK_ConsultasMedicos foreign key (crm) references medicos(crm),
-constraint FK_ConsultasPacientes foreign key (cpf) references paciente(cpf));
+constraint FK_ConsultasPacientes foreign key (cpf) references pacientes(cpf));
 
 
